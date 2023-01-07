80b47d5bdd1c9580027bd0d5a708f206339903e4
wumingke <766190553@qq.com>
2023-01-07 16:29:12 +0800

    add

---
+++ language/test1.js
@@ -0,0 +1,26 @@
+var name = "mingke"
+var book = {
+    topic: "java",
+    fat: true
+}
+book.fat
+
+book.id = 1
+
+function abs(x) {
+    if (x > 0) {
+        return x;
+    } else {
+        return -x;
+    }
+}
+
+// 11
+/**
+ * 222
+ */
+
+/*
+11
+*/
+
